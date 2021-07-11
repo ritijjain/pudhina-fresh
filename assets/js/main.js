4d5f8fab06596783c488800388ace39a77cf2988
Ritij Jain <ritij@ritijjain.com>
2021-07-11 13:53:47 +0530

    Added JavaScript to reverse darkmode on img tags

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,23 @@ function switch_dark_mode_icon(darkmode, id) {
 }
 
 function render_dark_mode_icon(darkmode, id) {
-    console.log(darkmode.isActivated())
+    var x = document.getElementsByTagName("img");
+    var i;
+
+    console.log(x)
     if (darkmode.isActivated()) {
         document.getElementById(id).classList.remove('fa-sun');
         document.getElementById(id).classList.add('fa-moon');
+        for (i = 0; i < x.length; i++) {
+            x[i].style["mix-blend-mode"] = "difference";
+        }
 
     } else {
         document.getElementById(id).classList.remove('fa-moon');
         document.getElementById(id).classList.add('fa-sun');
+        for (i = 0; i < x.length; i++) {
+            x[i].style["mix-blend-mode"] = "normal";
+        }
     }
 }
+
--- index.md
+++ index.md
@@ -15,6 +15,17 @@ Pudhina Fresh is free and open-source. Checkout [Pudhina Fresh](https://github.c
 
 <br>
 <br>
+
+Posts
+{% for post in site.posts %}
+  <a href="#">{{ post.title }}</a>
+{% endfor %}
+
+Projects
+{% for project in site.data.projects %}
+  <a href="#">{{ project.name }}</a>
+{% endfor %}
+
 <br>
 <br>
 <br>
